2bedf975953ae02b9ea626bbafbe72141a722b00
Mohammad Nakhaee <55943631+mohammadnakhaee@users.noreply.github.com>
2026-06-24 14:52:25 +0000

    Added voila action

--- src/test/apps/__init__.py
+++ src/test/apps/__init__.py
@@ -1,5 +1,16 @@
 from nomad.config.models.plugins import AppEntryPoint
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
+from nomad.config.models.ui import (
+    App,
+    Column,
+    Columns,
+    FilterMenu,
+    FilterMenus,
+    RowActionNorth,
+    RowActions,
+    Rows,
+)
+
+schema_name = 'nomad_hysprint.schema_packages.hysprint_package.HySprint_VoilaNotebook'
 
 app_entry_point = AppEntryPoint(
     name='NewApp',
@@ -18,6 +29,16 @@ app_entry_point = AppEntryPoint(
             options={
                 'material': FilterMenu(label='Material'),
             }
+        ),
+        rows=Rows(
+            actions=RowActions(
+                options={
+                    'launch': RowActionNorth(
+                        tool_name='voila',
+                        filepath=f'data.notebook_file#{schema_name}',
+                    )
+                }
+            ),
         ),
     ),
 )
